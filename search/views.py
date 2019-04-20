2eecdfc176d6f059852327884ef39440bdc47678
brynne-farrell <notkilroy@protonmail.com>
2019-04-19 23:56:21 -0400

    added server side data rendering

--- search/views.py
+++ search/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from prideflagsearch.settings import DATA_FILE
+import json
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'search/index.html')
+    dataPath = DATA_FILE
+    sourceData = {'flags': [] }
+    with open(dataPath) as dataFile:
+        sourceData = json.load(dataFile)
+
+    sourceData['flags'].sort(key=lambda x: x['name'])
+        
+    flagCount = len(sourceData['flags'])
+    cleanedData = []
+    for i in range(0,flagCount, 4):
+        thisRow = []
+        for j in range(0, 4):
+            if (flagCount > i + j):
+                thisRow.append(sourceData['flags'][i + j])
+        cleanedData.append(thisRow)
+        
+    flagWord = 'flags'
+    
+    if 1 == flagCount:
+        flagWord = 'flag'
+    
+    context = {
+        'flagData': cleanedData,
+        'flagCount': flagCount,
+        'flagWord': flagWord,
+    }
+    return render(request, 'search/index.html', context)
