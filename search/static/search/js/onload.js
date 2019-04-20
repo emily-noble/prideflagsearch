ea74257f2c19f934126e7e03ea943f8e43b031f8
brynne-farrell <notkilroy@protonmail.com>
2019-04-19 23:59:18 -0400

    uncommented code

--- search/static/search/js/onload.js
+++ search/static/search/js/onload.js
@@ -1,27 +1,27 @@
-// "use strict";
+"use strict";
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     fetch("static/search/data/prideflags.json").then(function(data) {
-//         return data.json();
-//     }).then(function(data) {
-//         const flagList = data.flags;
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("static/search/data/prideflags.json").then(function(data) {
+        return data.json();
+    }).then(function(data) {
+        const flagList = data.flags;
 
-//         const display = new PFS.ResultDisplay(document.querySelector("#searchResults"));
-//         const searchApp = new PFS.FlagSearchApp(flagList, display);
+        const display = new PFS.ResultDisplay(document.querySelector("#searchResults"));
+        const searchApp = new PFS.FlagSearchApp(flagList, display);
 
-//         const searchForm = new PFS.SearchForm();
-//         searchForm.addFormChangeCallback((filter) => searchApp.search(filter));
+        const searchForm = new PFS.SearchForm();
+        searchForm.addFormChangeCallback((filter) => searchApp.search(filter));
 
-//         searchApp.search();
+        searchApp.search();
         
-//         const javaScriptRequiredList = document.querySelectorAll(".jx-javascript-required");
+        const javaScriptRequiredList = document.querySelectorAll(".jx-javascript-required");
         
-//         javaScriptRequiredList.forEach((element) => {
-//             if (element.classList.contains("hidden")) {
-//                 element.classList.remove("hidden");
-//             } else {
-//                 element.classList.add("hidden");
-//             }
-//         });
-//     });
-// })
+        javaScriptRequiredList.forEach((element) => {
+            if (element.classList.contains("hidden")) {
+                element.classList.remove("hidden");
+            } else {
+                element.classList.add("hidden");
+            }
+        });
+    });
+})
