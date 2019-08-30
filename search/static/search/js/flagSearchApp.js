4891a6bfcea15753552b4be3d2a266037146087f
brynne-farrell <notkilroy@protonmail.com>
2019-08-29 21:59:27 -0400

    Cleanup flag search code

--- search/static/search/js/flagSearchApp.js
+++ search/static/search/js/flagSearchApp.js
@@ -33,85 +33,36 @@
             
             this.flagList.forEach((flagElement) => {
                 flagElement.classList.remove("hidden");
-            });
-
-            let filteredFlagList = this.flagList;
-
-            // Search by shapes
-            const shouldHaveShapes = filters.shapeFilter;
-            if (null !== shouldHaveShapes) {
-                this.flagList.forEach((flagElement) => {
+                
+                // Search by shapes
+                if (null !== filters.shapeFilter) {
                     const hasShapes = flagElement.dataset.shapes === "True";
-                    if (shouldHaveShapes !== hasShapes) {
+                    if (filters.shapeFilter !== hasShapes) {
                         flagElement.classList.add("hidden");
                     }
-                });
-//                 filteredFlagList = filteredFlagList.filter((flag) => {
-//                     return flag.shapes === shouldHaveShapes;
-//                 });
-            }
+                }
 
-            // Search by stripes
-            const numberOfStripes = filters.stripeFilter;
-            if (numberOfStripes && !isNaN(numberOfStripes)) {
-                this.flagList.forEach((flagElement) => {
+                // Search by stripes
+                if (filters.stripeFilter && !isNaN(filters.stripeFilter)) {
                     const stripeCount = parseInt(flagElement.dataset.stripes, 10);
-                    if (numberOfStripes !== stripeCount && !isNaN(stripeCount)) {
+                    if (filters.stripeFilter !== stripeCount && !isNaN(stripeCount)) {
                         flagElement.classList.add("hidden");
                     }
-                });
-//                 filteredFlagList = filteredFlagList.filter((flag) => {
-//                     return flag.stripes === numberOfStripes;
-//                 });
-            }
+                }
 
-            // Search by colors
-            const requiredColors = filters.colorFilter;
-            if (requiredColors.length) {
-                this.flagList.forEach((flagElement) => {
+                // Search by colors
+                if (filters.colorFilter.length) {
                     const colorList = JSON.parse(flagElement.dataset.colors);
-                    const differenceList = requiredColors.filter((i) => {
+                    const differenceList = filters.colorFilter.filter((i) => {
                         return colorList.indexOf(i) < 0;
                     });
-                    
+
                     if (differenceList.length) {
                         flagElement.classList.add("hidden");
                     }
-                });
-//                 filteredFlagList = filteredFlagList.filter((flag) => {
-//                     const differenceList = requiredColors.filter((i) => {
-//                         return flag.colors.indexOf(i) < 0;
-//                     });
-
-//                     return 0 === differenceList.length;
-//                 });
-            }
-
-//             filteredFlagList = this.sortFlagList(filteredFlagList);
-
-//             this.flagDisplay.update(filteredFlagList);
-        }
-
-        /**
-         * Sorts a list of flags alphabetically
-         * @private
-         * @param {array} flagList
-         * @returns {array}
-         */
-        sortFlagList(flagList) {
-            const sortedFlagList = flagList.sort((a, b) => {
-                if (a.name < b.name) {
-                    return -1;
                 }
-
-                if (a.name > b.name) {
-                    return 1;
-                }
-
-                return 0;
             });
 
-            return sortedFlagList;
         }
     }
 
