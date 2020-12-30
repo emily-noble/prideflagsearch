7f9139871873e1360034bcfdee4d82520fa38f9f
brynne-farrell <notkilroy@protonmail.com>
2020-12-29 21:01:31 -0500

    Restarted and having better luck

--- search/static/search/js/onload.js
+++ search/static/search/js/onload.js
@@ -1,27 +1,31 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("static/search/data/prideflags.json").then(function(data) {
-        return data.json();
-    }).then(function(data) {
-        const flagList = data.flags;
-
-        const display = new PFS.ResultDisplay(document.querySelector("#searchResults"));
-        const searchApp = new PFS.FlagSearchApp(document.querySelectorAll(".flag-card"), display);
-
-        const searchForm = new PFS.SearchForm();
-        searchForm.addFormChangeCallback((filter) => searchApp.search(filter));
-
-//         searchApp.search();
-        
-        const javaScriptRequiredList = document.querySelectorAll(".jx-javascript-required");
-        
-        javaScriptRequiredList.forEach((element) => {
-            if (element.classList.contains("hidden")) {
-                element.classList.remove("hidden");
-            } else {
-                element.classList.add("hidden");
-            }
-        });
+    document.querySelectorAll(".jx-disclosure").forEach((disclosureButton) => {
+        new PFS.ExclusiveDisclosure(disclosureButton);
     });
+    
+//     fetch("static/search/data/prideflags.json").then(function(data) {
+//         return data.json();
+//     }).then(function(data) {
+//         const flagList = data.flags;
+
+//         const display = new PFS.ResultDisplay(document.querySelector("#searchResults"));
+//         const searchApp = new PFS.FlagSearchApp(document.querySelectorAll(".flag-card"), display);
+
+//         const searchForm = new PFS.SearchForm();
+//         searchForm.addFormChangeCallback((filter) => searchApp.search(filter));
+
+// //         searchApp.search();
+        
+//         const javaScriptRequiredList = document.querySelectorAll(".jx-javascript-required");
+        
+//         javaScriptRequiredList.forEach((element) => {
+//             if (element.classList.contains("hidden")) {
+//                 element.classList.remove("hidden");
+//             } else {
+//                 element.classList.add("hidden");
+//             }
+//         });
+//     });
 })
