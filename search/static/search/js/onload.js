8b87e9378aa37cca53b1c8d84260480db87ccb79
brynne-farrell <notkilroy@protonmail.com>
2021-01-02 22:29:59 -0500

    Made flag details button hidden when no javascript

--- search/static/search/js/onload.js
+++ search/static/search/js/onload.js
@@ -1,26 +1,30 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
+    // Enable javascript-only features
+    const javaScriptRequiredList = document.querySelectorAll(".jx-javascript-required");
+    javaScriptRequiredList.forEach((element) => {
+        if (element.classList.contains("hidden")) {
+            element.classList.remove("hidden");
+        } else {
+            element.classList.add("hidden");
+        }
+    });
+    
+    // Wire up citation disclosures
     document.querySelectorAll(".jx-disclosure").forEach((disclosureButton) => {
         new PFS.ExclusiveDisclosure(disclosureButton);
     });
     
-    const javaScriptRequiredList = document.querySelectorAll(".jx-javascript-required");
     
-    javaScriptRequiredList.forEach((element) => {
-            if (element.classList.contains("hidden")) {
-                element.classList.remove("hidden");
-            } else {
-                element.classList.add("hidden");
-            }
-        });
-    
+    // Create the search app (handles updating display)
     const searchApp = new PFS.FlagSearchApp(
         document.querySelectorAll(".jx-flag-card"),
         document.querySelector("#resultsCount"),
         document.querySelector("#resultsWord")
     );
 
+    // Create search form (listens to and emits filter changes)
     const searchForm = new PFS.SearchForm();
     searchForm.addFormChangeCallback((filter) => searchApp.search(filter));
 })
